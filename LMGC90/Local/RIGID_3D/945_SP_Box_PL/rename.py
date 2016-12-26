e741bff437d21d6437ef02dfff0116484b4c0deb
Vincent Acary <vincent.acary@inria.fr>
2016-12-26 18:54:20 +0100

    [*] first import os scripts

---
+++ LMGC90/Local/RIGID_3D/945_SP_Box_PL/rename.py
@@ -0,0 +1,23 @@
+import os
+import shutil as shutil
+from glob import glob 
+base = '945_SP_Box_PL'
+if not os.path.exists(base):
+    os.mkdir(base)
+counter =0
+for filename in glob('*.hdf5'):
+    print( filename)
+    #statinfo = os.stat(os.path.join(dirname, filename))
+    #print statinfo.st_size,  statinfo_previous.st_size
+    new_filename = filename.replace('FC3D',base)
+    if counter%10==0 :
+        print "copy", filename, "in ", os.path.join(base, new_filename) 
+        shutil.copy(filename, os.path.join(base, new_filename))
+    counter =counter +1
+
+for filename in glob('*.hdf5'):
+    print( filename)
+    if 'i05000'in filename:
+        new_filename = filename.replace('FC3D',base)
+        print "copy", filename, "in ", os.path.join(base, new_filename) 
+        shutil.copy(filename, os.path.join(base, new_filename))
