680e0ec94d9a7772d6e8fee034650ba1e9536b69
Vincent Acary <vincent.acary@inria.fr>
2019-04-08 16:43:03 +0200

    [Global] replace input.dat by mkinput.py

---
+++ siconos/Global/Kaplas_2400/kaplas_with_trace_global.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+
+#
+# Example of one object under gravity with one contactor and a ground
+#
+
+from siconos.mechanics.collision.tools import Contactor
+from siconos.io.mechanics_run import MechanicsHdf5Runner
+import siconos.numerics as Numerics
+import siconos.kernel as Kernel
+from siconos.io.FrictionContactTrace import GlobalFrictionContactTraceParams
+
+
+import sys
+sys.path.append('../../')
+
+from creation.tools import InputData
+
+from params import *
+
+# Creation of the hdf5 file for input/output
+with MechanicsHdf5Runner() as io:
+
+    input_data= InputData(io)
+    
+    # Definition of a non smooth law. As no group ids are specified it
+    # is between contactors of group id 0.
+    io.add_Newton_impact_friction_nsl('contact', mu=mu)
+
+
+hstep = h
+dump_probability = .02
+
+
+import os
+
+base = './KaplasTower'
+cmp=0
+output_dir_created = False
+output_dir = base +'_0'
+while (not output_dir_created):
+    print('output_dir', output_dir)
+    if (os.path.exists(output_dir)):
+        cmp =cmp+1
+        output_dir = base +  '_' + str(cmp)
+    else:
+        os.mkdir(output_dir)
+        output_dir_created = True
+
+fileName = os.path.join(output_dir,'KaplasTower')
+title = "Kaplas Tower"
+description = """
+Kaplas stacking with Bullet collision detection
+Moreau TimeStepping Global: h={0}, theta = {1}
+One Step non smooth problem: {2}, maxiter={3}, tol={4}
+""".format(hstep, theta, Numerics.solver_options_id_to_name(solver),
+           itermax,
+           tolerance)
+
+mathInfo = ""
+
+friction_contact_trace_params = GlobalFrictionContactTraceParams(
+    dump_itermax=1, dump_probability=None,
+    fileName=fileName, title=title,
+    description=description, mathInfo=mathInfo)
+
+# Run the simulation from the inputs previously defined and add
+# results to the hdf5 file. The visualisation of the output may be done
+# with the vview command.
+with MechanicsHdf5Runner(mode='r+') as io:
+
+    # By default earth gravity is applied and the units are those
+    # of the International System of Units.
+    # Because of fixed collision margins used in the collision detection,
+    # sizes of small objects may need to be expressed in cm or mm.
+    io.run(with_timer=True,
+           gravity_scale=1,
+           t0=0,
+           T=20.0,
+           h=hstep,
+           theta=theta,
+           Newton_max_iter=NewtonMaxIter,
+           set_external_forces=None,
+           solver=solver,
+           itermax=itermax,
+           tolerance=tolerance,
+           numerics_verbose=True,
+           output_frequency=None,
+           osi=Kernel.MoreauJeanGOSI,
+           friction_contact_trace=True,
+           friction_contact_trace_params=friction_contact_trace_params)
---
+++ siconos/Global/Kaplas_2400/params.py
@@ -0,0 +1,26 @@
+import siconos.numerics as Numerics
+
+t0 = 0
+T = 10
+h = 0.0005
+g = 9.81
+theta = 0.50001
+mu = 0.3
+dump_itermax = 80
+dump_probability = .02
+itermax = 100000
+NewtonMaxIter = 20
+tolerance = 1e-8
+solver = Numerics.SICONOS_GLOBAL_FRICTION_3D_ADMM
+
+# fileName = "KaplasTower"
+# title = "KaplasTower"
+# description = """
+# A Kapla Tower with Bullet collision detection
+# Moreau TimeStepping: h={0}, theta = {1}
+# One Step non smooth problem: {2}, maxiter={3}, tol={4}
+# """.format(h, theta, Numerics.idToName(solver),
+#            itermax,
+#            tolerance)
+
+# mathInfo = ""
