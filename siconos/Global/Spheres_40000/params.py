f057237e81a2a6332f22b874c752a18a17d9f444
Vincent ACARY <vincent.acary@inria.fr>
2022-02-24 18:17:16 +0100

    [Global] update w.r.t 4.5.0

--- siconos/Global/Spheres_40000/params.py
+++ siconos/Global/Spheres_40000/params.py
@@ -1,4 +1,5 @@
-import siconos.numerics as Numerics
+import siconos.numerics as sn
+import siconos.kernel as sk
 
 t0 = 0
 T = 30
@@ -6,23 +7,18 @@ h = 0.0005
 g = 9.81
 theta = 0.50001
 mu = 0.7
-dump_itermax = 1
+
+dump_itermax = 80
 dump_probability = .05
+
 itermax = 100
-NewtonMaxIter = 20
+NewtonMaxIter = 1
 tolerance = 1e-8
-solver = Numerics.SICONOS_GLOBAL_FRICTION_3D_ADMM
-#solver = Numerics.SICONOS_GLOBAL_FRICTION_3D_NSGS_WR
+
+solver_id = sn.SICONOS_GLOBAL_FRICTION_3D_ADMM
+options = sk.solver_options_create(solver_id)
+options.iparam[sn.SICONOS_IPARAM_MAX_ITER] = itermax
+options.dparam[sn.SICONOS_DPARAM_TOL] = tolerance
+
+
 multipointIterations = False
-
-# fileName = "Spheres"
-# title = "Spheres"
-# description = """
-# Spheres falling on the ground with Bullet collision detection
-# Moreau TimeStepping: h={0}, theta = {1}
-# One Step non smooth problem: {2}, maxiter={3}, tol={4}
-# """.format(h, theta, Numerics.idToName(solver),
-#            itermax,
-#            tolerance)
-
-# mathInfo = ""
