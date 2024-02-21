042fb0524ecffe9745abe1d6a2dc25d63a90ca00
Vincent ACARY <vincent.acary@inria.fr>
2024-02-21 10:22:39 +0100

    [siconos/Global] improve test script

--- siconos/test_H_matrix.py
+++ siconos/test_H_matrix.py
@@ -1,19 +1,15 @@
 import h5py
 from scipy.sparse import coo_matrix
 #filename = 'Global/siconos/PrimitiveSoup/PrimitiveSoup-i080-247-7.hdf5'
-mypath = 'Global/siconos/PrimitiveSoup/'
 
-#mypath = 'Global/siconos/KaplasTower/'
+
 mypath = 'Global/siconos/Spheres/'
-#mypath = 'Global/siconos/Capsules/'
-#mypath = 'Global/siconos/Chute/'
-#mypath = 'Global/siconos/Spheres1mm/'
-#mypath = 'Global/siconos/BoxStacks/'
-#mypath = './Global/siconos/'
-
+mypath = 'Global/Spheres_1mm/'
+mypath='/scratch/vincent/fclib-library/Global/siconos/Spheres/'
+mypath='/scratch/vincent/fclib-library/Global/siconos/Chute/'
 import numpy as np
 
-def test_size(filename, key):
+def test_H_ONB(filename, key):
 
     f = h5py.File(filename,'r')
     #print('test size for ', key)
@@ -57,13 +53,13 @@ def test_size(filename, key):
                 ok =True
                 tol =1e-04
 
-                # print('norm n',np.linalg.norm(n))
-                # print('norm t',np.linalg.norm(t))
-                # print('norm s',np.linalg.norm(s))
+                print('norm n',np.linalg.norm(n))
+                print('norm t',np.linalg.norm(t))
+                print('norm s',np.linalg.norm(s))
                 
-                # print('n.s',np.dot(n,s))
-                # print('n.t',np.dot(n,t))
-                # print('s.t',np.dot(s,t))
+                print('n.s',np.dot(n,s))
+                print('n.t',np.dot(n,t))
+                print('s.t',np.dot(s,t))
                 
                 error = max(np.dot(n,s),np.dot(t,s))
                 error = max(error,np.dot(n,t))
@@ -92,7 +88,48 @@ def test_size(filename, key):
     #assert(x.shape[0] == nz)
     return ok
  
+def test_w_in_range_of_H(filename, key):
 
+    f = h5py.File(filename,'r')
+    #print('test size for ', key)
+    # print(f.keys())
+    if not ('fclib_global' in f.keys()):
+        return False
+        
+    
+    #print(f['fclib_global'][key].keys())
+    nz = f['fclib_global'][key]['nz'][0]
+    m = f['fclib_global'][key]['m'][0]
+    n = f['fclib_global'][key]['n'][0]
+    
+    # print('nz',nz)
+    # print('m',m)
+    # print('n',n)
+    nzmax = f['fclib_global'][key]['nzmax'][0]
+    # print('nzmax',nzmax)
+    x = f['fclib_global'][key]['x'][:]
+    idx = f['fclib_global'][key]['i'][:]
+    jdx = f['fclib_global'][key]['p'][:]
+
+    H = coo_matrix((x, (idx, jdx)), shape=(m,n) ).toarray().transpose()
+    #print(H, H.shape)
+
+    
+    w = f['fclib_global']['vectors']['w'][:]
+    if np.linalg.norm(w) > 1e-10:
+        print(np.linalg.norm(w))
+
+    else:
+        print('w is null')
+        return True
+
+    #HHT = np.dot(H,H.T)
+    #print(HHT)
+    
+        
+    
+    return True
+ 
     
 import os
 from os import walk
@@ -109,7 +146,8 @@ for (dirpath, dirnames, filenames) in walk(mypath):
         if ext == ".hdf5":
             print('\nfilename', os.path.join(dirpath,f))
             #test_size(os.path.join(dirpath,f), 'M')
-            ok = test_size(os.path.join(dirpath,f), 'H')
+            #ok = test_H_ONB(os.path.join(dirpath,f), 'H')
+            ok = test_w_in_range_of_H(os.path.join(dirpath,f), 'H')
             if not ok:
                 test.append(os.path.join(dirpath,f))
 
